fa2baf6d878876e71bf928f891927bbb722583b3
Bartosz Mediger <bartosz@mediger.net>
2018-09-28 11:22:33 +0200

    Fluid height of topbar, button

--- src/js/framework.js
+++ src/js/framework.js
@@ -1,4 +1,8 @@
+import ScrollToPlugin from 'gsap/ScrollToPlugin';
+
+
 document.addEventListener('DOMContentLoaded',function() {
+
 
 // isMobile
 
@@ -9,6 +13,74 @@ document.addEventListener('DOMContentLoaded',function() {
 	    document.getElementsByTagName('html')[0].className += ' desktop';
 	}
 })();
+
+
+    // GoToTarget
+
+    (function() {
+    
+        'use strict';
+        
+        const scrollTo = function (target, speed, offset) {    
+    		TweenLite.to(window, speed, {
+    			scrollTo: {
+    				y: target + offset,
+    				autoKill: false
+    			},
+    			ease: Power1.easeOut
+    		});
+    	};
+    
+    	const speed_calculate = function (target) {
+        	let base_speed  = 25,
+        		page_offset = window.pageYOffset || document.documentElement.scrollTop,
+            	offset_diff = Math.abs(target - page_offset),
+            	speed = ((offset_diff * base_speed) / 1000)/100;
+    
+        	return speed;
+    	};
+    
+    	const clickAction = function(e) {
+    	
+    	    const that = e.currentTarget;
+    
+    	    let src = that.getAttribute('href'),
+    	        window_pos = window.pageYOffset || window.scrollY || document.documentElement.scrollTop;
+    
+    	    const obj = document.getElementById( src.slice(1, src.length) );
+    
+    	    if (obj) {
+    	        let offset = that.getAttribute('data-offset');
+    
+                if (!offset) {
+                    offset = 0;
+                }
+                
+                document.body.removeAttribute('style');
+    	    
+    	        let target = window_pos + obj.getBoundingClientRect().top - offset;
+    	        scrollTo(target, speed_calculate(target), offset);
+    	    }
+            
+            if (window.e) {
+                window.e.returnValue = false;
+            }
+            
+    	    e.preventDefault() ? e.preventDefault() : e.preventDefault = false;
+    	};
+    	
+    
+        
+        const btn = document.getElementsByClassName('js-goto');
+        
+        if (btn.length>0) {
+        	for (let i = 0; i < btn.length; i++) {
+                btn[i].addEventListener('click', clickAction);
+            }
+        }
+    })();
+    
+
 
 
     // Nav
